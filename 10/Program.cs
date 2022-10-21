a78c0f57763a461e991087d0fb89e1a593c98c3c
Максим <Mt.Pm@yandex.ru>
2022-10-21 14:26:48 +0700

    Записал задачу 10

--- 10/Program.cs
+++ 10/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿string str = "";
+int a;
+
+Console.WriteLine("");
+do
+{
+    Console.Write("Введите число: ");
+    while (int.TryParse(str = Console.ReadLine()!, out a) == false) 
+    {
+
+        Console.WriteLine("err: несоответствие типу integer!");
+        Console.Write("Введите число: ");
+    }
+
+    a = (int)Math.Abs(int.Parse(str));
+    if (a < 100 || a > 999)
+    {
+
+        Console.WriteLine("err: не трехзначное число!");
+
+    }
+} while (a < 100 || a > 999);
+
+a /= 10;
+a %= 10;
+
+Console.WriteLine("");
+Console.WriteLine("Цифра 2-го разряда: [" + a + "]");
